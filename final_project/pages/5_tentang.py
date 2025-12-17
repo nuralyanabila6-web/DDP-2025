ee2ebc04272e76424109daa03efe8b91d30f009e
alielryn <alielryn@gmail.com>
2025-12-17 08:53:07 +0700

    Revise application description and features list
    Updated application description and features.

--- final_project/pages/5_tentang.py
+++ final_project/pages/5_tentang.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.title("ℹ️ Tentang Aplikasi")
 
 st.markdown("""
-Aplikasi *Beressin* adalah aplikasi To-Do List sederhana yang dibuat untuk memenuhi Final Project  
-mata kuliah *Dasar-Dasar Pemrograman*.
+Aplikasi *Beressin* adalah aplikasi Task Manajer untuk mengelola tugas harian. Aplikasi ini dapat membantu user mengatur tugas agar tetap teratur*.
 
 Fitur:
 - Melihat daftar tugas
 - Menambah tugas
 - Menghapus tugas
 - Navigasi mudah antar halaman
+
 """)
